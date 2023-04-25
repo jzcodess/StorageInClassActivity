dee4ded66620e349376b9fc3badb20bf207d7d0e
jzcodess <tuq67092@temple.edu>
2023-04-25 02:48:31 -0400

    commit

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.networkapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,6 +15,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    private val myFile = "my_file"
+    private lateinit var file: File
+    private var save = false
+    private  lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +42,25 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
+        preferences = getPreferences(MODE_PRIVATE)
+        file = File(filesDir,myFile)
+
+        if(file.exists()){
+            try{
+                val br = BufferedReader(FileReader(file))
+                val text = StringBuilder()
+                var line: String?
+                while (br.readLine().also{line =it} != null){
+                    text.append(line)
+                    text.append('\n')
+                }
+                br.close()
+                downloadComic(text.toString())
+            }
+            catch(e: IOException){
+                e.printStackTrace()
+            }
+        }
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
@@ -46,7 +71,15 @@ class MainActivity : AppCompatActivity() {
     private fun downloadComic (comicId: String) {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
-            JsonObjectRequest(url, {showComic(it)}, {
+            JsonObjectRequest(url, {
+                try{
+                    val outputStream = FileOutputStream(file)
+                    outputStream.write(numberEditText.text.toString().toByteArray())
+                    outputStream.close()
+                }catch(e: Exception){
+                    e.printStackTrace()
+                }
+                showComic(it)}, {
             })
         )
     }
